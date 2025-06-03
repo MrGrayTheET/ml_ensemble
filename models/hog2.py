d798203423c881a88cc13dd71c5bdf358c09a616
MrGrayTheET <sandersnicholas68@gmail.com>
2025-06-03 18:08:02 +0000

    Removing unnecessary files

--- models/hog2.py
+++
@@ -1,7 +0,0 @@
-from statsmodels.tsa.seasonal import seasonal_decompose, STL, MSTL
-import yfinance as yf
-from feature_engineering import kama
-from model_prep import TrendModel as tmod
-
-asset = yf.download('HE=F', multi_level_index=False)
-new_seasonal = (21,49,122)
